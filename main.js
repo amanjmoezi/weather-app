1a3fd917c05e447198b861cd02899581c96d7bad
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-04-25 14:10:05 +0430

    add vars in main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const tocken = "5ee8b69845b8fa7570ecf552330f9eff";
+const city = "sanandaj";
+const tempElement = document.querySelector(".temp");
+const statusElement = document.querySelector(".status");
+const timeElement = document.querySelector(".time");
+const countryElement = document.querySelector(".loc");
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${tocken}&units=metric`;
