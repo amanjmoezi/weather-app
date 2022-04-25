3b25006e248c3d1167903f08355f782404c30313
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-04-25 14:11:52 +0430

    get data form openweathermap.org with main.js

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ const statusElement = document.querySelector(".status");
 const timeElement = document.querySelector(".time");
 const countryElement = document.querySelector(".loc");
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${tocken}&units=metric`;
+fetch(url)
+.then(resp=>resp.json())
